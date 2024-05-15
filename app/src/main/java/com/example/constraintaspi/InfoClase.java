0453981ef6ef0bdd3c116231a45ed2b9d6fc9d0c
Cescobar332 <cescobar332@unab.edu.co>
2024-05-15 18:24:23 -0500

    Añadido de archivos
    Proyecto de AS

---
+++ app/src/main/java/com/example/constraintaspi/InfoClase.java
@@ -0,0 +1,17 @@
+package com.example.constraintaspi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class InfoClase extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+    }
+
+}
---
+++ app/src/main/java/com/example/constraintaspi/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.constraintaspi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+    }
+    public void ejecutar_info(View view){
+        Intent i = new Intent(this, InfoClase.class);
+        startActivity(i);
+    }
+    public void salirApp(View view){
+        finish();
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu mimenu){
+        getMenuInflater().inflate(R.menu.menu_en_activity, mimenu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem opcion_menu) {
+        int id = opcion_menu.getItemId();
+        if(id == R.id.configuracion){
+            return true;
+        } else if (id == R.id.info) {
+            ejecutar_info(null);
+            return true;
+        }
+        return super.onOptionsItemSelected(opcion_menu);
+    }
+}
